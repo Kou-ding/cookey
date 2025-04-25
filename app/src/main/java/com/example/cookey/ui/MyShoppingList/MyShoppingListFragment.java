aae2ae5a9416000b0d92d5c1ce3386d6ea477435
Kou-ding <fotispapadak@gmail.com>
2025-04-26 00:29:40 +0300

    Refactor MyShoppingListFragment and layout
    - Removed unused parameters and methods in `MyShoppingListFragment`.
    - Updated `fragment_my_shopping_list.xml` for better layout, including ScrollView, button positions, TextViews.
    - added food and non-food TextViews

--- app/src/main/java/com/example/cookey/ui/MyShoppingList/MyShoppingListFragment.java
+++ app/src/main/java/com/example/cookey/ui/MyShoppingList/MyShoppingListFragment.java
@@ -26,53 +26,15 @@ import android.widget.Spinner;
 import com.example.cookey.DBHandler;
 import com.example.cookey.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyShoppingListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyShoppingListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public MyShoppingListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyShoppingListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyShoppingListFragment newInstance(String param1, String param2) {
-        MyShoppingListFragment fragment = new MyShoppingListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
