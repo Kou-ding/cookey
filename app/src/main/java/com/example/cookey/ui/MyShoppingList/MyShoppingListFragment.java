57f9ab770757247a95690fcfc0f1511fbbf67f16
Kou-ding <fotispapadak@gmail.com>
2025-06-03 15:16:11 +0300

    Remove unused `discardPendingItems` method
    The `discardPendingItems` method, which was previously called in `addItem`, has been removed as it is no longer necessary. This method was responsible for saving any pending text in unfinished items before adding a new item.

--- app/src/main/java/com/example/cookey/ui/MyShoppingList/MyShoppingListFragment.java
+++ app/src/main/java/com/example/cookey/ui/MyShoppingList/MyShoppingListFragment.java
@@ -213,9 +213,6 @@ public class MyShoppingListFragment extends Fragment {
         }
     }
     public void addItem(boolean foodMode) {
-        // First save any pending text in unfinished items
-        discardPendingItems();
-
         try (DBHandler db = new DBHandler(requireContext(), null, null, 1)) {
             if(foodMode) {
                 db.addFoodItem(db.getNextUnusedShoppingListItemId());
@@ -226,17 +223,6 @@ public class MyShoppingListFragment extends Fragment {
         loadItemsFromDB();
     }
 
-    private void discardPendingItems() {
-        for (int i = 0; i < adapter.getItemCount(); i++) {
-            ShoppingListItem item = adapter.getItems().get(i);
-            if (item.getShoppingListItemName() == null || item.getShoppingListItemName().isEmpty()) {
-                // This is an unfinished item - remove it
-                try (DBHandler db = new DBHandler(requireContext(), null, null, 1)) {
-                    db.deleteShoppingListItem(item.getShoppingListItemId());
-                }
-            }
-        }
-    }
     public void checkAllItems(){
         // Save on the database
         try (DBHandler db = new DBHandler(getContext(), null, null, 1)) {
